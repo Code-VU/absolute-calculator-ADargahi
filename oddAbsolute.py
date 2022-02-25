f5e79d47a7dee40bcbc8b039b745b55173754a76
Abbas Dargahi <adargahi97@gmail.com>
2022-02-25 10:08:15 -0600

    Assignment 4.5.1

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,7 +2,13 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    if int(in_num) > 21:
+        diff = (int(in_num) % 21) * 2
+    else:
+        diff = 21 % int(in_num)
+    print(diff)
     # end assignment
+
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
